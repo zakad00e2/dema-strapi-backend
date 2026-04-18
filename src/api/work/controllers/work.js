33cd253ceb3205bd2e0098a89222cf3829cfbe85
zakaria safi <zakaria@gmail.com>
2026-04-18 21:25:23 +0200

    Add default population settings and merge logic for work controller

--- src/api/work/controllers/work.js
+++ src/api/work/controllers/work.js
@@ -2,4 +2,70 @@
 
 const { factories } = require('@strapi/strapi');
 
-module.exports = factories.createCoreController('api::work.work');
+const DEFAULT_POPULATE = {
+  mainImage: true,
+  gallery: true,
+  localizations: true,
+  preEventMarketingPoints: {
+    populate: {
+      points: true,
+      images: true,
+    },
+  },
+  postEventMarketingPoints: {
+    populate: {
+      points: true,
+      images: true,
+    },
+  },
+  launchEventExperiencePoints: true,
+  campaignImpactPoints: true,
+};
+
+const isPlainObject = (value) =>
+  Object.prototype.toString.call(value) === '[object Object]';
+
+const mergePopulate = (currentPopulate, defaultPopulate) => {
+  if (!currentPopulate || currentPopulate === '*') {
+    return { ...defaultPopulate };
+  }
+
+  if (!isPlainObject(currentPopulate)) {
+    return { ...defaultPopulate };
+  }
+
+  const merged = { ...currentPopulate };
+
+  for (const [key, value] of Object.entries(defaultPopulate)) {
+    if (!(key in merged)) {
+      merged[key] = value;
+      continue;
+    }
+
+    if (isPlainObject(merged[key]) && isPlainObject(value)) {
+      merged[key] = mergePopulate(merged[key], value);
+    }
+  }
+
+  return merged;
+};
+
+module.exports = factories.createCoreController('api::work.work', () => ({
+  async find(ctx) {
+    ctx.query = {
+      ...ctx.query,
+      populate: mergePopulate(ctx.query?.populate, DEFAULT_POPULATE),
+    };
+
+    return await super.find(ctx);
+  },
+
+  async findOne(ctx) {
+    ctx.query = {
+      ...ctx.query,
+      populate: mergePopulate(ctx.query?.populate, DEFAULT_POPULATE),
+    };
+
+    return await super.findOne(ctx);
+  },
+}));
